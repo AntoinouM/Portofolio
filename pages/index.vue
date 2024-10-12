f4c9e8971298404c3ea00973dde9ad5fed85d253
Antoine Muneret <antoine.muneret@hotmail.fr>
2024-10-12 17:33:46 +0200

    new img dor stack and json

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,5 @@
 <script  setup>
+    import stack from '~/public/Tech_Logos/stack.json'
 
 </script>
 
@@ -12,15 +13,20 @@
                     <article class="bento__item bento__picture">
                         <img src="/Pictures/climbing.JPG" alt="man climbing">
                     </article>
-                    <article class="bento__item bento__quote"></article>
+                    <article class="bento__item bento__quote">
+                        <p class="body">"Whether it’s climbing or coding, I’m all about solving problems, finding the best path, and reaching the goal step by step."</p>
+                    </article>
                     <article class="bento__item bento__position">
                         <h3 class="title4 position_text">Full-stack developer & UX enthusiast</h3>
                     </article>
                     <article class="bento__item bento__name">
-                        <img src="/public/Pictures/cosmo.png" alt="" class="name__image">
                         <h2 class="title4 name__name">Hello, I am <span class="title2 name_name--strong">ANTOINE MUNERET</span></h2>
+                        <img src="/public/Pictures/cosmo.png" alt="astronaute" class="name__image">
                     </article>
-                    <article class="bento__item bento__stack"></article>
+                    <article class="bento__item bento__stack">
+                        <h3 class="title3 stack__title">My stack</h3>
+                        <div class="stack__container"></div>
+                    </article>
                     <article class="bento__item bento__education"></article>
                 </div>
             </div>
