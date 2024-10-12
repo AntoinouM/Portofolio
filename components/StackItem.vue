7e153ec9a0c4283ece6aa92a00d4cf65b62c980a
Antoine Muneret <antoine.muneret@hotmail.fr>
2024-10-12 18:46:09 +0200

    stak progress

--- components/StackItem.vue
+++ components/StackItem.vue
@@ -16,9 +16,9 @@
 
 <template>
     <div class="stackItem__container">
-        <img src="/public/Pictures/climbing.JPG" :alt="name" class="stackItem__logo">
+        <img :src="src" :alt="name" class="stackItem__logo">
         <span class="stackItem__tooltip">
-            <h3 class="title4 tooltip__name">name</h3>
+            <h3 class="title4 tooltip__name">{{name}}</h3>
             <p class="body tooltip__experience"></p>
         </span>
     </div>
@@ -30,16 +30,42 @@
 
         &__container {
             position: relative;
-            width: 15%;
+            width: clamp(36px, 14%, 45px);
             height: auto;
             aspect-ratio: 1/1;
-            min-width: 36px;
         }
 
         &__logo {
             width: 100%;
             height: 100%;
             object-position: center center;
+            filter: grayscale(.9);
+            transition: all .5s ease-out;
+
+            &:hover {
+                filter: grayscale(0);
+            }
+        }
+
+        &__tooltip {
+            $tooltip-height: 60px;
+            $tooltip-width: 120px;
+
+            position: absolute;
+            height: $tooltip-height;
+            width: $tooltip-width;
+            border: $border-width solid $primary;
+            top: $tooltip-height * -1 + $spacer-1 * -1;
+            left: 50%;
+            transform: translateX(-50%);
+            display: none;
+            background-color: $grey-90;
+
+            z-index: 10;
+        }
+
+        &__logo:hover + .stackItem__tooltip {
+            display: block;
         }
     }
 
