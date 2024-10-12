baaacd1ebfd893267bf51f63cbe474dcd243cae5
Antoine Muneret <antoine.muneret@hotmail.fr>
2024-10-12 16:38:07 +0200

    new image

--- pages/index.vue
+++ pages/index.vue
@@ -9,13 +9,15 @@
             <div class="bento__container">
                 <div class="bento__grid">
                     <article class="bento__item bento__presentation"></article>
-                    <article class="bento__item bento__picture"></article>
+                    <article class="bento__item bento__picture">
+                        <img src="/Pictures/climbing.JPG" alt="man climbing">
+                    </article>
                     <article class="bento__item bento__quote"></article>
                     <article class="bento__item bento__position">
                         <h3 class="title4 position_text">Full-stack developer & UX enthusiast</h3>
                     </article>
                     <article class="bento__item bento__name">
-                        <img src="" alt="" class="name__image">
+                        <img src="/public/Pictures/cosmo.png" alt="" class="name__image">
                         <h2 class="title4 name__name">Hello, I am <span class="title2 name_name--strong">ANTOINE MUNERET</span></h2>
                     </article>
                     <article class="bento__item bento__stack"></article>
